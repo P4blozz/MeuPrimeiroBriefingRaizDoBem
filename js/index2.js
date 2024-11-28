d96277cbc1b6acdb36422704d06f6bfaa5151196
Luiz Eduardo <Luizinformaticaeduardo@gmail.com>
2024-11-28 08:52:11 -0300

    card swipper adicionado.

--- js/index2.js
+++ js/index2.js
@@ -1,31 +1,26 @@
-const swiper = new Swiper('.card-wrapper', {
+document.addEventListener('DOMContentLoaded', function () {
+  const swiper = new Swiper('.swiper-wrapper', {
     loop: true,
-    spaceBetween: 30, // Corrected the typo here
-    
-    // Pagination bullet
+    spaceBetween: 30,
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
       dynamicBullets: true,
     },
-  
-    // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  
-    // Responsive breakpoints
     breakpoints: {
       0: {
-        slidesPerView: 1, // Show 1 slide at a time for small screens
+        slidesPerView: 1,
       },
       768: {
-        slidesPerView: 2, // Show 2 slides at a time for tablets or larger screens
+        slidesPerView: 2,
       },
       1024: {
-        slidesPerView: 3, // Show 3 slides at a time for desktop or larger screens
+        slidesPerView: 3,
       },
     }
   });
-  
+});
